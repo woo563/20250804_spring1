a67bc4157863dc0ba17863ffabb44db328446190
LeeGu-hun <v_star@naver.com>
2025-08-06 16:48:31 +0900

    0805

--- ex2/src/main/java/com/example/ex2/controller/MemberController.java
+++ ex2/src/main/java/com/example/ex2/controller/MemberController.java
@@ -1,7 +1,14 @@
 package com.example.ex2.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/member")
 public class MemberController {
+
+  @RequestMapping("/join")
+  public void join() {
+
+  }
 }
