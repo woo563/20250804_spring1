ed205b7e2c965b5fcad60970ef7fed4e8b48adbb
LeeGu-hun <v_star@naver.com>
2025-08-06 16:04:00 +0900

    0806

--- ex2/src/main/java/com/example/ex2/Ex2Application.java
+++ ex2/src/main/java/com/example/ex2/Ex2Application.java
@@ -8,6 +8,7 @@ public class Ex2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ex2Application.class, args);
+		System.out.println("http://localhost:8080/ex2");
 	}
 
 }
---
+++ ex2/src/main/java/com/example/ex2/controller/MemberController.java
@@ -0,0 +1,7 @@
+package com.example.ex2.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+}
