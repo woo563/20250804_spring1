a657a45fb093e813db3f5c2c8e6d3a6da1831c13
woo563 <chchtkfkd200@gmail.com>
2025-08-11 10:19:39 +0900

    0811

--- ex2/src/main/java/com/example/ex2/controller/MemberController.java
+++ ex2/src/main/java/com/example/ex2/controller/MemberController.java
@@ -3,16 +3,20 @@ package com.example.ex2.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// template engine을 사용하면 @Controller를 필수로 사용!
 @Controller
 @RequestMapping("/member")
 public class MemberController {
 
+  // void일 경우 요청된 url이 resource 위치와 동일
   @RequestMapping("/join")
   public void join() {
 
   }
-  @RequestMapping("/loginPrc")
+
+  // String일 경우 요청된 url을 대신하여 임의로 지정가능
+  @RequestMapping("/login")
   public String login() {
-    return "loginPrc";
+    return "/member/loginPrc";
   }
 }
